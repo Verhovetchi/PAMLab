f5130ad97a1d6850dbd007b4329e3b73ac7c6b00
Verhovetchi Daniel <daniel.verhovetchi@ati.utm.md>
2019-11-12 22:05:38 +0200

    Add Android Project Loading2

---
+++ Loading2/app/src/main/java/com/example/loading/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.loading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView loading = (ImageView)findViewById(R.id.imageView);
+        animation = (AnimationDrawable)loading.getDrawable();
+    }
+
+    public void start(View v){
+        animation.start();
+    }
+
+    public void stop(View v){
+        animation.stop();
+    }
+}
